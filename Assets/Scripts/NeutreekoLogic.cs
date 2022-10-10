8160bf3a8fb07d4ea60b3d43f6966e3116a71ee7
davidrocha9 <up201806371@fe.up.pt>
2022-10-10 18:52:34 +0200

    Initial commit

---
+++ Assets/Scripts/NeutreekoLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeutreekoLogic : MonoBehaviour
+{
+    public GameObject r1, r2, r3;
+    public GameObject b1, b2, b3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
